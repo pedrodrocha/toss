786225d79fd2a655a3a3c773ce58436334355617
Pedro Diniz Rocha <pedro.diniz.rocha@gmail.com>
2026-09-12 11:57:03 -0300

    Fix context validation propagation

--- lua/toss/context.lua
+++ lua/toss/context.lua
@@ -4,14 +4,29 @@ local function invalid(message)
   return nil, message
 end
 
-function M.capture()
+local function assert_buffer_is_file()
   if vim.bo.buftype ~= "" then
     return invalid("current buffer is not a file")
   end
+end
 
-  local absolute_path = vim.api.nvim_buf_get_name(0)
+local function assert_is_file_path(absolute_path)
   if absolute_path == "" then
     return invalid("current buffer has no file path")
+  end
+end
+
+function M.capture()
+  local _, err = assert_buffer_is_file()
+  if err then
+    return nil, err
+  end
+
+  local absolute_path = vim.api.nvim_buf_get_name(0)
+
+  _, err = assert_is_file_path(absolute_path)
+  if err then
+    return nil, err
   end
 
   local root = vim.fs.root(0, ".git") or vim.fn.getcwd()
