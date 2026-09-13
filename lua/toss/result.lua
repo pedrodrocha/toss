c7f9c4293bc0261c43336831e71e3abd9ef46097
Pedro Diniz Rocha <pedro.diniz.rocha@gmail.com>
2026-09-13 18:25:39 -0300

    Add Result predicate methods

--- lua/toss/result.lua
+++ lua/toss/result.lua
@@ -1,10 +1,14 @@
 ---@class TossOk<T>
 ---@field kind "ok"
 ---@field value T
+---@field is_ok fun(self: TossOk<T>): boolean
+---@field is_err fun(self: TossOk<T>): boolean
 
 ---@class TossErr
 ---@field kind "err"
 ---@field error TossError
+---@field is_ok fun(self: TossErr): boolean
+---@field is_err fun(self: TossErr): boolean
 
 ---@alias TossResult<T> TossOk<T>|TossErr
 
@@ -15,23 +19,41 @@
 
 local M = {}
 
+local result_methods = {}
+
+---@param self TossResult<any>
+---@return boolean
+function result_methods.is_ok(self)
+  return self.kind == "ok"
+end
+
+---@param self TossResult<any>
+---@return boolean
+function result_methods.is_err(self)
+  return self.kind == "err"
+end
+
+local result_metatable = {
+  __index = result_methods,
+}
+
 ---@generic T
 ---@param value T|nil
 ---@return TossOk<T>
 function M.ok(value)
-  return {
+  return setmetatable({
     kind = "ok",
     value = value,
-  }
+  }, result_metatable)
 end
 
 ---@param error_value TossError
 ---@return TossErr
 function M.err(error_value)
-  return {
+  return setmetatable({
     kind = "err",
     error = error_value,
-  }
+  }, result_metatable)
 end
 
 ---@param value any
