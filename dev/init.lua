3ade9c7b36daff6962a43935effaf4707aa6fa6a
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2026-09-19 17:06:10 -0300

    Task 052: add Telescope file-browser mapping helper (#45)

--- dev/init.lua
+++ dev/init.lua
@@ -26,6 +26,24 @@ package.path = table.concat({
 local toss = require("toss")
 toss.setup({ transport = "auto", mappings = true, which_key = true })
 
+-- Configure the optional Telescope file-browser integration when Telescope is
+-- available. This only affects the current development session.
+local telescope_ok, telescope = pcall(require, "telescope")
+if telescope_ok then
+  local file_browser_mappings = require("toss.integrations.telescope.file_browser").mappings()
+
+  telescope.setup({
+    extensions = {
+      file_browser = {
+        initial_mode = "normal",
+        mappings = file_browser_mappings,
+      },
+    },
+  })
+
+  pcall(telescope.load_extension, "file_browser")
+end
+
 vim.notify(
   "toss: development source loaded from " .. repository_root .. " (transport: auto, mappings, which-key: optional)",
   vim.log.levels.INFO
